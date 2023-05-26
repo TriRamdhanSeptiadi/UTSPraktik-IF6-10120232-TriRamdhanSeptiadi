3a443ce520552268456502f891774f5d77aea047
TriRamdhanSeptiadi <todoraki240@gmail.com>
2023-05-26 17:54:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if6_10120232_triramdhanseptiadi/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.utspraktik_if6_10120232_triramdhanseptiadi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//Nama  : Tri Ramdhan Septiadi
+//NIM   : 10120232
+//Kelas : IF-6
+//Tanggal Pengerjaan : 26 Mei 2023
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
